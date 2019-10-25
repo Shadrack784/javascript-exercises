062aadac41fce9e291881bf4f61e665f456c8146
shadrack tush <jeanclush12632gmail.com>
2019-10-25 16:03:31 +0300

    undefined type

--- 1-basics/basics.js
+++ 1-basics/basics.js
@@ -40,8 +40,8 @@ describe("basics", function() {
             ////////////////////////////////////////////////////////////////////
             // Declare "someVariable" in way so that the declaration is NOT hoisted.
             // ✏️ ADD CODE HERE ✏️
-
-            ////////////////////////////////////////////////////////////////////
+            let someVariable = undefined;
+            //////////////////////////////////////////////////////////////////
 
             expect(someVariable).to.equal(undefined);
         });
@@ -51,7 +51,7 @@ describe("basics", function() {
         it("should run without errors", function() {
             ////////////////////////////////////////////////////////////////////
             // ✏️ ADD CODE HERE ✏️
-
+            let someVariable;
             ////////////////////////////////////////////////////////////////////
 
             expect(typeof someVariable).to.equal("undefined");
